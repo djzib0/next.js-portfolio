f904d1f5d4be3003bf6ce0a4e6f6e7dc1702062d
djzib0 <piotrsabatura@gmail.com>
2025-11-24 19:44:18 +0100

    added folders hotspot, fixed background flickering, added tooltips

--- components/centerContainer/CenterContainer.tsx
+++ components/centerContainer/CenterContainer.tsx
@@ -1,18 +1,50 @@
 'use client'
-import React from 'react'
+import Head from 'next/head'
+import React, { useEffect } from 'react'
 
 const CenterContainer = () => {
-
 
   const sayHello = () => {
     console.log("hello")
   }
 
+  // Preload and decode images to prevent first-hover flicker
+  useEffect(() => {
+    const images = [
+      '/img/bg-desk.png',
+      '/img/bg-desk-select-computer.png',
+      '/img/bg-desk-select-picture.png',
+      '/img/bg-desk-select-folders.png',
+    ]
+
+    images.forEach((src) => {
+      const img = new Image()
+      img.src = src
+      if (img.decode) img.decode().catch(() => {}) // force decode
+    })
+  }, [])
+
   return (
     <div className='background-image m-auto'>
-      {/* <div className="hotspot-computer debug"></div> */}
-      <div className="hotspot-computer debug" onClick={sayHello}></div>
-      <div className="hotspot-picture debug" onClick={sayHello}></div>
+      <Head>
+        <link rel="preload" as="image" href="/img/bg-desk.png" />
+        <link rel="preload" as="image" href="/img/bg-desk-select-computer.png" />
+        <link rel="preload" as="image" href="/img/bg-desk-select-picture.png" />
+        <link rel="preload" as="image" href="/img/bg-desk-select-folders.png" />
+      </Head>
+
+      <div className='hotspot-computer debug' onClick={sayHello}></div>
+      <div className='hotspot-picture debug' onClick={sayHello}></div>
+      <div className='hotspot-folders debug' onClick={sayHello}></div>
+      <div className='hotspot-computer-desc'>
+        <p className='text-start2P'>My stack</p>
+      </div>
+      <div className='hotspot-picture-desc'>
+        <p className='text-start2P'>About me</p>
+      </div>
+      <div className='hotspot-folders-desc'>
+        <p className='text-start2P'>Projects</p>
+      </div>
     </div>
   )
 }
